1454fed244719d1085af16ad2579f0438110c00a
Marcel Kanne <marcel.kanne@outlook.com>
2018-10-09 12:39:39 +0200

    FileIo

--- src/main/scala/de/htwg/se/durak/model/FieldComponent/FieldBaseImpl/Field.scala
+++ src/main/scala/de/htwg/se/durak/model/FieldComponent/FieldBaseImpl/Field.scala
@@ -1,10 +1,49 @@
 package de.htwg.se.durak.model.FieldComponent.FieldBaseImpl
 
 import de.htwg.se.durak.model.FieldComponent.FieldInterface
-import de.htwg.se.durak.model.PlayerInterface
+import de.htwg.se.durak.model.FieldComponent.PlayerInterface
 
 import scala.collection.mutable.ArrayBuffer
+import scala.xml.{Elem, Node}
 
-case class Field(p: Position) extends FieldInterface {
+class Field extends FieldInterface {
+  amountOfPlayer = 2
   players = ArrayBuffer[PlayerInterface]()
+  field = new Field
+
+  def createNewGame: Unit = {
+    amountOfPlayer = 2
+    players = ArrayBuffer[PlayerInterface]()
+    field = new Field
+  }
+
+  def toXml: Elem = {
+    return <game>
+    <field>{serializeTiles(this.field.tiles)}</field>
+    </game>
+  }
+
+  def serializeTiles(tiles: Array[Tile]): String = {
+    val stringBuilder = new StringBuilder
+
+    for (i <- tiles.indices) {
+      stringBuilder.append(tiles(i).value)
+      if (i != (tiles.length - 1)) {
+        stringBuilder.append(",")
+      }
+    }
+    stringBuilder.toString()
+  }
+
+  def fromXml(node: Node): Unit = {
+    val playerCardOnHand = new ArrayBuffer[Card]()
+    playerCardOnHand = (node).text.toString
+
+    val enemyCardOnHand = new ArrayBuffer[Card]()
+    enemyCardOnHand = (node).text.toString
+
+    val deck = new ArrayBuffer[Card]()
+    deck = (node).text.toString
+
+  }
 }
--- src/main/scala/de/htwg/se/durak/model/FieldComponent/FieldInterface.scala
+++ src/main/scala/de/htwg/se/durak/model/FieldComponent/FieldInterface.scala
@@ -1,21 +1,33 @@
 package de.htwg.se.durak.model.FieldComponent
 
-import de.htwg.se.durak.model.PlayerInterface
+import de.htwg.se.durak.model.FieldComponent.FieldBaseImpl.Field
+import scala.xml.{Elem, Node}
 
 import scala.collection.mutable.ArrayBuffer
 
 trait FieldInterface {
   var players: ArrayBuffer[PlayerInterface] = _
-  var amountOfPlayer: Int = 0
-  val length: Int = 3
+  var amountOfPlayer: Integer
+  var field: Field
 
-  def createNewField: FieldInterface
+  def createNewGame
+
+  def left(x: Integer)
+
+  def right(x: Integer)
+
+  def up(y: Integer)
+
+  def down(y: Integer)
+
+  def toXml: Elem
+
+  def fromXml(node: Node)
 }
 
 trait DeckInterface[Card] {
-
+  def isEmpty(): Boolean
 }
 
 trait PlayerInterface {
-
 }
