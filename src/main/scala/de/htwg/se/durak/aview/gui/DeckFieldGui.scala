55dd4dc1c2dc6fbab1681314949048fef2e76c61
odsource <christoph.kaiser93@outlook.de>
2018-10-20 16:06:51 +0200

    Gui displayHand() implementiert

---
+++ src/main/scala/de/htwg/se/durak/aview/gui/DeckFieldGui.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.durak.aview.gui
+
+import java.awt.GridLayout
+
+import javax.swing.{BorderFactory, BoxLayout, JPanel}
+
+class DeckFieldGui extends JPanel{
+  val deckPanel = new JPanel()
+  setLayout(new GridLayout(1, 3))
+  setBorder(BorderFactory.createEtchedBorder())
+
+  add(deckPanel)
+}
